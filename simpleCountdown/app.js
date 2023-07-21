3192cd3a17fb1df98713d766c4007d9f5de6fc96
Bateyjosue <josuebatey19@gmail.com>
2023-07-21 17:11:45 +0200

    Simple Countdown Layout and Style

---
+++ simpleCountdown/app.js
@@ -0,0 +1,43 @@
+const container = document.querySelector(".container__todo__list");
+
+const paintList = (data) =>{
+
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const spanRemove = document.createElement("span");
+    const input = document.createElement("input");
+
+    input.setAttribute("type", "checkbox");
+    
+    span.innerHTML = `
+    <input type="checkbox" name="done" class="done">
+    ${data}
+    `
+    li.appendChild(span);
+    spanRemove.setAttribute("class","material-symbols-outlined")
+    spanRemove.textContent = `delete_forever`
+    li.appendChild(spanRemove)
+
+    ul.appendChild(li);
+    spanRemove.addEventListener("click", function() {
+        this.parentNode.remove();
+    })
+
+}
+
+const unPaintList = (event) => {
+    event.target.parentElement.remove();
+}
+
+const ul = document.querySelector('.todoList');
+const input = document.querySelector('#task');
+const btnAdd = document.querySelector("#submit");
+
+btnAdd.addEventListener("click", (event) => {
+    event.preventDefault();
+    const getValue  = input.value
+    if(getValue){
+        paintList(getValue);
+        input.value = "";
+    }
+});
