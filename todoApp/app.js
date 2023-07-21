e1b0eeeb2a7e9fc227ed3a0ff398869327fdc909
Bateyjosue <josuebatey19@gmail.com>
2023-07-21 15:08:16 +0200

    Desesign Today and paint element on add action

--- todoApp/app.js
+++ todoApp/app.js
@@ -1,8 +1,64 @@
 const container = document.querySelector(".container__todo__list");
 
+
+
+const paintList = (data) =>{
+    ul.innerHTML += `<li>
+        <span>${data}</span>
+        <span class="material-symbols-outlined">delete_forever</span>
+    </li>`
+}
+
+const unPaintList = (event) => {
+    const li = event.target.remove();
+}
 container.innerHTML =`
 <form action="" class="">
     <input type="text" name="task" id="task" placeholder="Add to your List">
     <input type="submit" value="Add" id="submit">
-</form> 
-`
+</form>
+    <ul class="todoList">
+        <li>
+            <span>
+                <input type="checkbox" name="done" class="done">
+                Go home earlier
+            </span>
+            <span class="material-symbols-outlined">delete_forever</span>
+        </li>
+        <li>
+        <span>
+            <input type="checkbox" name="done" class="done">
+            Go home earlier
+        </span>
+        <span class="material-symbols-outlined">delete_forever</span>
+    </li>
+    <li>
+    <span>
+        <input type="checkbox" name="done" class="done">
+        Go home earlier
+    </span>
+    <span class="material-symbols-outlined">delete_forever</span>
+</li>
+<li>
+<span class="is__done">
+    <input type="checkbox" name="done" class="done">
+    Go home earlier
+</span>
+<span class="material-symbols-outlined">delete_forever</span>
+</li>
+    </ul>
+`;
+
+const ul = document.querySelector('.todoList');
+const input = document.querySelector('#task');
+const btnAdd = document.querySelector("#submit");
+
+
+btnAdd.addEventListener("click", (event) => {
+    event.preventDefault();
+    const getValue  = input.value
+    if(getValue){
+        paintList(getValue);
+        input.value = "";
+    }
+});
