7da445eef8b2f5fd0a3eabe474b01cf7f5e33a33
Bateyjosue <josuebatey19@gmail.com>
2023-07-21 16:32:35 +0200

    Fix Remove

--- todoApp/app.js
+++ todoApp/app.js
@@ -1,44 +1,33 @@
 const container = document.querySelector(".container__todo__list");
 
-
 const paintList = (data) =>{
 
     const li = document.createElement("li");
     const span = document.createElement("span");
-    const span1 = document.createElement("span");
-    const input = document.createElement("input");
-    input.setAttribute("type", "checkbox");
-    input.setAttribute("class", "done")
+    const spanRemove = document.createElement("span");
     
     span.innerHTML = `
     <input type="checkbox" name="done" class="done">
     ${data}
     `
     li.appendChild(span);
-    span1.setAttribute("class","material-symbols-outlined")
-    span1.textContent += `delete_forever`
-    li.appendChild(span1)
-    // li.innerHTML += ` <span class="material-symbols-outlined">delete_forever</span>  `
+    spanRemove.setAttribute("class","material-symbols-outlined")
+    spanRemove.textContent = `delete_forever`
+    li.appendChild(spanRemove)
 
     ul.appendChild(li);
+    spanRemove.addEventListener("click", function() {
+        this.parentNode.remove();
+    })
 }
 
 const unPaintList = (event) => {
-    event.target.remove();
+    event.target.parentElement.remove();
 }
-// container.innerHTML =`
-//     <form action="" class="">
-//         <input type="text" name="task" id="task" placeholder="Add to your List">
-//         <input type="submit" value="Add" id="submit">
-//     </form>
-//     <ul class="todoList"></ul>
-// `;
 
 const ul = document.querySelector('.todoList');
 const input = document.querySelector('#task');
 const btnAdd = document.querySelector("#submit");
-const btnDelete = document.querySelectorAll('.todoList li span:nth-child(2)')
-
 
 btnAdd.addEventListener("click", (event) => {
     event.preventDefault();
@@ -48,9 +37,3 @@ btnAdd.addEventListener("click", (event) => {
         input.value = "";
     }
 });
-
-console.log(btnDelete);
-[...btnDelete].forEach(btn => {
-})
-
-console.log(todo);
