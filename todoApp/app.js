2bcefcfb833d0b2534660f001b5541372a4ce9f9
Bateyjosue <josuebatey19@gmail.com>
2023-07-21 15:51:26 +0200

    Design Remove btn

--- todoApp/app.js
+++ todoApp/app.js
@@ -1,57 +1,43 @@
 const container = document.querySelector(".container__todo__list");
 
 
-
 const paintList = (data) =>{
-    ul.innerHTML += `<li>
-        <span>${data}</span>
-        <span class="material-symbols-outlined">delete_forever</span>
-    </li>`
+
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const span1 = document.createElement("span");
+    const input = document.createElement("input");
+    input.setAttribute("type", "checkbox");
+    input.setAttribute("class", "done")
+    
+    span.innerHTML = `
+    <input type="checkbox" name="done" class="done">
+    ${data}
+    `
+    li.appendChild(span);
+    span1.setAttribute("class","material-symbols-outlined")
+    span1.textContent += `delete_forever`
+    li.appendChild(span1)
+    // li.innerHTML += ` <span class="material-symbols-outlined">delete_forever</span>  `
+
+    ul.appendChild(li);
 }
 
 const unPaintList = (event) => {
-    const li = event.target.remove();
+    event.target.remove();
 }
-container.innerHTML =`
-<form action="" class="">
-    <input type="text" name="task" id="task" placeholder="Add to your List">
-    <input type="submit" value="Add" id="submit">
-</form>
-    <ul class="todoList">
-        <li>
-            <span>
-                <input type="checkbox" name="done" class="done">
-                Go home earlier
-            </span>
-            <span class="material-symbols-outlined">delete_forever</span>
-        </li>
-        <li>
-        <span>
-            <input type="checkbox" name="done" class="done">
-            Go home earlier
-        </span>
-        <span class="material-symbols-outlined">delete_forever</span>
-    </li>
-    <li>
-    <span>
-        <input type="checkbox" name="done" class="done">
-        Go home earlier
-    </span>
-    <span class="material-symbols-outlined">delete_forever</span>
-</li>
-<li>
-<span class="is__done">
-    <input type="checkbox" name="done" class="done">
-    Go home earlier
-</span>
-<span class="material-symbols-outlined">delete_forever</span>
-</li>
-    </ul>
-`;
+// container.innerHTML =`
+//     <form action="" class="">
+//         <input type="text" name="task" id="task" placeholder="Add to your List">
+//         <input type="submit" value="Add" id="submit">
+//     </form>
+//     <ul class="todoList"></ul>
+// `;
 
 const ul = document.querySelector('.todoList');
 const input = document.querySelector('#task');
 const btnAdd = document.querySelector("#submit");
+const btnDelete = document.querySelectorAll('.todoList li span:nth-child(2)')
 
 
 btnAdd.addEventListener("click", (event) => {
@@ -62,3 +48,9 @@ btnAdd.addEventListener("click", (event) => {
         input.value = "";
     }
 });
+
+console.log(btnDelete);
+[...btnDelete].forEach(btn => {
+})
+
+console.log(todo);
