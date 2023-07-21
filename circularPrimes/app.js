9aaea48ae41b67dd25263165e1e0ae8346e0afb9
Bateyjosue <josuebatey19@gmail.com>
2023-07-21 19:05:20 +0200

    Solve Cercular Primes

--- circularPrimes/app.js
+++ circularPrimes/app.js
@@ -1,40 +1,40 @@
-const screenCounter = document.querySelector(".screen__counter");
-const btnFirst = document.querySelector('.actions span:nth-child(1)')
-const btnMiddle = document.querySelector('.actions span:nth-child(2)')
-const btnLast = document.querySelector('.actions span:nth-child(3)')
 
-btnFirst.addEventListener('click', () => {
-    screenCounter.textContent = 5;
-})
-
-btnMiddle.addEventListener('click', () => {
-    screenCounter.textContent = 10;
-
-})
-
-btnLast.addEventListener('click', () => {
-    screenCounter.textContent = 15;
-
-})
-
-let x;
-
-if(screenCounter.textContent > 0) {
-    x = setInterval(() => {
-        screenCounter.textContent = screenCounter.textContent - 1;
-        if(screenCounter.textContent == 0) {
-            clearInterval(x)
-            // x = null
-            screenCounter.textContent = 0
-            btnFirst.style['display'] = 'block';
-            btnMiddle.style['display'] = 'block';
-            btnLast.style['display'] = 'block';
-        }else {
-            btnFirst.style['display'] = 'none';
-            btnMiddle.style['display'] = 'none';
-            btnLast.style['display'] = 'none';
-
-        }
-
-    }, 1000)
+function isPrime(n) {
+  if (n == 1) return false;
+  for (var i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i == 0) return false;
+  }
+  return true;
 }
+
+function rotate(n) {
+  var nn = ""+n;
+  var n2 = nn[nn.length-1]+nn.substring(0,nn.length-1);
+  return parseInt(n2);
+}
+
+function circular(N) {
+  var outbound = 0;
+  for (var i = 2; i <= N; i++) {
+    var isMatch = true;
+    var check = i;
+    for (j = 0; j < (""+i).length; ++j) {
+      if (!isPrime(check)) {
+        isMatch = false;
+        break;
+      }
+      check = rotate(check);
+    }
+    if (isMatch) outbound++;
+  }
+  return outbound;
+}
+
+const input = document.querySelector(".number");
+const result = document.querySelector(".result");
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", (e) => {
+    e.preventDefault();
+  result.innerHTML = circular(input.value);
+});
