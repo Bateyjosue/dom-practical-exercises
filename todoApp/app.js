32a2220ed5210659687d09d89adf21b83d1704b6
Bateyjosue <josuebatey19@gmail.com>
2023-07-21 13:04:11 +0200

    Add Todolist Layout and basic Style

---
+++ todoApp/app.js
@@ -0,0 +1,8 @@
+const container = document.querySelector(".container__todo__list");
+
+container.innerHTML =`
+<form action="" class="">
+    <input type="text" name="task" id="task" placeholder="Add to your List">
+    <input type="submit" value="Add" id="submit">
+</form> 
+`
